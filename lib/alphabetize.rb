fe99772580c1fa801001f4f93f7d6cfdf8210b38
Rene Avila <reneavila1993@gmail.com>
2020-05-16 16:56:05 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 require 'pry'
 def alphabetize(arr)
-  alphabet = ["a", "b", "c", "ĉ", "d", "e", "f", "g", "ĝ", "h", "ĥ", "i", "j", "ĵ", "k", "l", "m", "n", "o", "p", "r", "s", "ŝ", "t", "u", "ŭ", "v", "z"]
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz".split('')
   arr.sort_by do |word|
     word.split('').collect do |letter|
     alphabet.index(letter)
